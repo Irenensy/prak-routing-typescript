e6a985f7db062be822778a713f4abd063408336e
Irenensy <h1101241031@student.untan.ac.id>
2026-03-07 06:34:47 +0000

    latihan mandiri

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,59 @@
-import http from "http";
+import http, { Server } from "http";
 
+const products = [
+     { id: 1, name: "Laptop"},
+     { id: 2, name: "Mouse"},
+];
+
+const users = [
+     { id: 1, name: "Irene"},
+     { id: 2, name: "Nasya"},
+     { id: 3, name: "Azalia"},
+]
 const server = http.createServer((req, res)=>{
-    const url = req.url;
-    const method = req.method;
-    //routing manual
-    if(url === "/"&& method === "GET"){
-        res.writeHead(200, {"Content-type":"application/json"});
-        res.end(JSON.stringify({message:"Selamat datang di halaman Home"}));
-    }else if(url ==="/about"&& method === "GET"){
-        res.writeHead(200, {"Content-type":"application/json"});
-        res.end(JSON.stringify({message:"Halaman About"}));
-    }else if(url?.startsWith("/user")&& method === "GET"){
-        const id = url.split("/")[2];
-        res.writeHead(200, {"Content-type":"application/json"});
-        res.end(JSON.stringify({message:"User ID:" + id}));
-    }else{
-        res.writeHead(404, {"Content-type":"application/json"});
-        res.end(JSON.stringify({message:"Route tidak ditemukan"}));
-    }
-});
-const PORT = 3000;
-server.listen(PORT, ()=>{
+     const url = req.url || "";
+     const method = req.method;
+     console.log (`[${new Date().toLocaleTimeString()}] ${method} ${url}`);
+
+     // routing manual
+     if(url === "/"&& method === "GET"){
+          res.writeHead(200,{"content-type":"application/json"});
+          res.end(JSON.stringify({message:"Selamat datang dihalaman Home"}));
+     }else if(url ==="/about"&& method ==="GET"){
+          res.writeHead(200,{"content-type":"application/json"});
+          res.end(JSON.stringify({message: "Halaman About"}));
+
+     }else if(url ==="/products"&& method === "GET"){
+               res.writeHead(200,{"content-type":"application/json"});
+               res.end(JSON.stringify(products));
+
+     }else if (url === "/products" && method === "POST"){
+          res.writeHead(200, {"content-type":"application/json"});
+          res.end(JSON.stringify({message: "Product Berhasil ditambahkan"}));
+
+     }else if(url?.startsWith("/users/")&& method === "GET"){
+          const id = Number(url.split("/")[2]);
+          const user = users.find((u) => u.id === id);
+
+          if (user){
+          res.writeHead(200, {"content-type":"application/json"});
+          res.end(JSON.stringify(user));
+     }else {
+          res.writeHead(200, {"content-type":"application/json"});
+          res.end(JSON.stringify({message: "User tidak ditemukan"}));
+          }
+     }
+     
+     else{
+          res.writeHead(200, {"content-type":"appliaction/json"});
+          res.end(JSON.stringify({message:"Router tidak ditemukan"}));
+     }
+     });
+
+     const startTime = Date.now();
+     const endTime = Date.now();
+     console.log(`Requesst selesai dalam ${endTime - startTime} ms`);
+
+server.listen(3000,()=>{
     console.log("Server Berjalan di http://localhost:3000");
 });
